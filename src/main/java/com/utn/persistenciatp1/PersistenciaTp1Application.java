6a5e70857628cf497251ecec1069d560c22c85ed
jazminrillo3 <jazmin.rillo@alumnos.frm.utn.edu.ar>
2023-09-05 20:05:16 -0300

    Creando proyecto

---
+++ src/main/java/com/utn/persistenciatp1/PersistenciaTp1Application.java
@@ -0,0 +1,13 @@
+package com.utn.persistenciatp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenciaTp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistenciaTp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/utn/persistenciatp1/PersistenciaTp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.persistenciatp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenciaTp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
