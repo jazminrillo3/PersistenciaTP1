3f0a26f9993cd4aabc5d0beba551e91be1a014c1
jazminrillo3 <jazmin.rillo@alumnos.frm.utn.edu.ar>
2023-09-06 11:12:45 -0300

    Agregando mostrarFactura y calcularTotal

--- src/main/java/com/utn/persistenciatp1/entities/Factura.java
+++ src/main/java/com/utn/persistenciatp1/entities/Factura.java
@@ -1,6 +1,8 @@
 package com.utn.persistenciatp1.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,11 +18,24 @@ public class Factura extends BaseEntidad {
     private String fecha;
     private int numero;
     private Double descuento;
+    @Enumerated(EnumType.STRING)
     private FormaPago formaPago;
-    private int total;
+    private Double total;
 
-    private enum FormaPago{
+    public enum FormaPago{
         EFECTIVO, MERCADO_PAGO, DEBITO, CREDITO,
+    }
+
+    public void calcularTotal(Pedido p){
+        total = p.getTotal() - descuento;
+    }
+
+    public void mostrarFactura(){
+        System.out.println("Fecha: " + fecha);
+        System.out.println("Número: " + numero);
+        System.out.println("Descuento: $" + descuento);
+        System.out.println("Forma de pago: " + formaPago);
+        System.out.println("Total: " + total);
     }
 
 }
